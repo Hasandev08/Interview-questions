e01942447a2af3392f826d39e9f52a80c94f5075
Hasan Ahmed <92812904+Hasandev08@users.noreply.github.com>
2022-12-02 19:40:29 +0500

    Update isPalindrome Linked List.cpp

--- Programs/isPalindrome Linked List.cpp
+++ Programs/isPalindrome Linked List.cpp
@@ -2,21 +2,21 @@ bool isPalin (Node* head)
 {
 	Node* temp = head;
 	
-	stack<int> s;
+	stack<int> s;		//creating a stack
 	
-	while (temp != NULL)
+	while (temp != NULL)		//traversing the linked list
 	{
-		s.push(temp->data);
+		s.push(temp->data);	//pushing the linked list data in the stack
 		temp = temp->next;
 	}
 	
 	while(head != NULL)
 	{
-		int i = s.top();
+		int i = s.top();	//taking the top element in the stack
 		
-		s.pop();
+		s.pop();		//removing the element from the stack
 		
-		if(head->data != i)
+		if(head->data != i)	//comparing the data in the head node with the top element of stack to check the palindrome
 		{
 			return false;
 		}
