8ea5bd795c346f63bdca023e25ed6ddcfade2c7e
Hasan Ahmed <92812904+Hasandev08@users.noreply.github.com>
2022-12-09 02:38:21 +0500

    Add files via upload

---
+++ Programs/Deleting Middle Node.cpp
@@ -0,0 +1,28 @@
+int getLength(Node* head)			//finding the length of the linked list
+{
+	int length = 0;
+	
+	while(head != NULL)
+	{
+		head = head->next;
+		length++;
+	}
+	
+	return length;
+}
+
+void midNode(Node* head)
+{
+	int len = getLength(head);
+	Node* temp = head;
+	
+	int mid = len / 2;			//finding the middle of the length of the linked list
+	
+	while (mid != 0)
+	{
+		temp = temp->next;		//traversing to the middle of the linked list
+		mid--;	
+	}
+	free(temp)
+	temp = NULL;
+}
